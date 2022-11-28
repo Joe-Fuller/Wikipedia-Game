611d73c58c7413076c00926493b3c27b147b2737
Joe <joefuller042@gmail.com>
2022-11-28 18:24:48 +0000

    update CurrentPage to make new links clickable

--- src/components/CurrentPage.jsx
+++ src/components/CurrentPage.jsx
@@ -1,24 +1,46 @@
 import { useEffect, useState } from "react";
+import alterLinks from "../utils/alterLinks";
 import getRandomPage from "../utils/getRandomPage";
+import getPage from "../utils/getPage";
 
 const CurrentPage = () => {
-  const [title, setTitle] = useState("No Title");
+  const [title, setTitle] = useState(null);
   const [htmlString, setHtmlString] = useState("No HTML String");
 
   useEffect(() => {
-    getRandomPage().then((body) => {
-      setTitle(body.lead.normalizedtitle);
-      setHtmlString(body.lead.sections[0].text);
-      for (let section of body.remaining.sections) {
-        setHtmlString((h) => h + section.text);
-      }
-    });
-  }, []);
+    if (title) {
+      console.log(title);
+      getPage(title).then((body) => {
+        setHtmlString(body.lead.sections[0].text);
+        for (let section of body.remaining.sections) {
+          setHtmlString((h) => h + section.text);
+        }
+      });
+    } else {
+      getRandomPage().then((body) => {
+        setTitle(body.lead.normalizedtitle);
+        setHtmlString(body.lead.sections[0].text);
+        for (let section of body.remaining.sections) {
+          setHtmlString((h) => h + section.text);
+        }
+      });
+    }
+  }, [title]);
+
+  const handleClick = (e) => {
+    const element = e.target.closest("B");
+    if (element && e.currentTarget.contains(element)) {
+      setTitle(element.innerText);
+    }
+  };
 
   return (
     <section>
       <h2>Current Page: {title}</h2>
-      <div dangerouslySetInnerHTML={{ __html: htmlString }} />
+      <div
+        onClick={handleClick}
+        dangerouslySetInnerHTML={{ __html: alterLinks(htmlString) }}
+      />
     </section>
   );
 };
