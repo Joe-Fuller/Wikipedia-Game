4e652adc398a7a63da0e168a16fd64297340757b
Joe <joefuller042@gmail.com>
2022-11-28 16:48:13 +0000

    add util to get a random page title

---
+++ src/utils/getTargetPage.js
@@ -0,0 +1,20 @@
+const getTargetPage = () => {
+  let headers = new Headers({
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    "API-User-Agent": "joefuller042@gmail.com",
+  });
+
+  return fetch("https://en.wikipedia.org/api/rest_v1/page/random/title", {
+    method: "GET",
+    headers: headers,
+  })
+    .then((res) => {
+      return res.json();
+    })
+    .then((body) => {
+      return body.items[0].title;
+    });
+};
+
+export default getTargetPage;
