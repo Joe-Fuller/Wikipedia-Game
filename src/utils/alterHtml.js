cff1322ee1e53f9c851bad0e274e2aa01576e559
Joe <joefuller042@gmail.com>
2022-11-29 17:12:49 +0000

    add function to remove expandable tables

--- src/utils/alterHtml.js
+++ src/utils/alterHtml.js
@@ -1,12 +1,14 @@
 const alterHtml = (htmlText) => {
   let alteredText = htmlText;
   alteredText = alterLinks(alteredText);
-  alteredText = removeSups(alteredText);
+  //alteredText = removeSups(alteredText);
+  alteredText = removeExpandableTables(alteredText);
   return alteredText;
 };
 
 const alterLinks = (htmlText) => {
-  const re = /<a.*?title="(.*?)".*?<\/a>/g;
+  //   const re = /<a.*?title="(.*?)".*?<\/a>/g;
+  const re = /<a href="\/.*?title="(.{0,30}?)".{0,30}?<\/a>/g;
 
   const alteredText = htmlText.replaceAll(re, "<b>$1</b>");
   return alteredText;
@@ -14,6 +16,13 @@ const alterLinks = (htmlText) => {
 
 const removeSups = (htmlText) => {
   const re = /<sup.*?>/g;
+
+  const alteredText = htmlText.replaceAll(re, "");
+  return alteredText;
+};
+
+const removeExpandableTables = (htmlText) => {
+  const re = /<table class="box-Expand_language[\s\S]*?<\/table>/g;
 
   const alteredText = htmlText.replaceAll(re, "");
   return alteredText;
