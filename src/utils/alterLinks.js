1e773da572ea13189571578b4f35a9a4da99bd7a
Joe <joefuller042@gmail.com>
2022-11-29 11:19:55 +0000

    remove solo alterLinks file

--- src/utils/alterLinks.js
+++
@@ -1,8 +0,0 @@
-const alterLinks = (htmlText) => {
-  const re = /<a.*?title="(.*?)".*?<\/a>/g;
-
-  const alteredText = htmlText.replaceAll(re, "<b>$1</b>");
-  return alteredText;
-};
-
-export default alterLinks;
