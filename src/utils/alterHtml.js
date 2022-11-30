37e38658c81a4c4cfef26886b5df158d49b4f813
Joe <joefuller042@gmail.com>
2022-11-30 10:06:35 +0000

    remove section links

--- src/utils/alterHtml.js
+++ src/utils/alterHtml.js
@@ -1,6 +1,7 @@
 const alterHtml = (htmlText) => {
   let alteredText = htmlText;
   alteredText = alterLinks(alteredText);
+  alteredText = removeSectionLinks(alteredText);
   alteredText = removeCitations(alteredText);
   alteredText = removeExpandableTables(alteredText);
   return alteredText;
@@ -29,6 +30,14 @@ const removeExpandableTables = (htmlText) => {
   const re = /<table class="box-Expand_language[\s\S]*?<\/table>/g;
 
   const alteredText = htmlText.replaceAll(re, "");
+  return alteredText;
+};
+
+const removeSectionLinks = (htmlText) => {
+  const re =
+    /<a.{0,60}?href="\/.{0,60}?title=".{0,60}?".{0,60}?>(.{0,60}?)<\/a>/g;
+
+  const alteredText = htmlText.replaceAll(re, "$1");
   return alteredText;
 };
 
