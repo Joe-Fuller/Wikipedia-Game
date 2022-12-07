cde1fabcab96331fbdcabc975d595e428b722012
Joe <joefuller042@gmail.com>
2022-12-07 15:52:13 +0000

    style dropdown

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -1,15 +1,29 @@
+import "../styles/sections.css";
+
 const Sections = ({ className, sections }) => {
+  const navToSelection = () => {
+    const el = document.getElementById("section_dropdown");
+    const val = el.options[el.selectedIndex].id;
+    window.location.href = val;
+  };
+
   return (
     <section className={className}>
-      {sections.map((section) => {
-        const sectionLink = `#${section.replaceAll(" ", "_")}`;
-        return (
-          <button key={section} id={section}>
-            {" "}
-            <a href={sectionLink}>{section}</a>
-          </button>
-        );
-      })}
+      <select className="dropdown-menu" id="section_dropdown">
+        {sections.map((section) => {
+          const sectionLink = `#${section.replaceAll(" ", "_")}`;
+          return (
+            <option
+              className="dropdown-item"
+              key={section}
+              id={sectionLink}
+              onClick={navToSelection}
+            >
+              {section}
+            </option>
+          );
+        })}
+      </select>
     </section>
   );
 };
